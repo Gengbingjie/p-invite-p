41cb249ba5ac024123d2e511c3ae02406fedfa0f
Liujiaxu <2236352497@qq.com>
2023-11-21 17:29:04 +0800

    领奖接口初步完成

--- netlify/koa/controller/api/collect.js
+++ netlify/koa/controller/api/collect.js
@@ -1,17 +1,72 @@
-const { outJson } = require('../../utils')
-const db = require('../../lib/mysql')
+const { outJson } = require('../../utils/utils');
+const { fulfillStatus, insertGiftLog, fulfillment } = require('../../utils/function');
+const db = require('../../lib/mysql');
 
 module.exports = async (ctx)=>{
     const userOpenId = ctx.state.userOpenId;
-    const userInfo = await db.readMysql(`SELECT * FROM preRegisterUser WHERE userOpenId="${userOpenId}"`);
+    let userInfo = {};
+    userInfo.invitationNum = undefined;
+    const skuId = 'LOL_HEXTECH_CHEST_KEY_SKU_2';
+    try{
+        userInfo = await db.readMysql(`SELECT * FROM preRegisterUser WHERE userOpenId="${userOpenId}"`);
+    }catch (err){
+        return ctx.body = outJson(ctx, 50001);
+    }
+    //会话是否过期
     if(!userInfo){
         return ctx.body = outJson(ctx, 40001);
     }
-    if(userInfo.invitationNum !== 3){
+    //查询是否满足领取条件
+    if(userInfo[0].invitationNum !== 3){
         return ctx.body = outJson(ctx, 20003);
     }
-    ctx.body = {
-        status: 'success',
-        message: 'Hello, frontend!',
+    //查询是否已领取
+    const checkSkuCollect = await db.readMysql(`SELECT * FROM prePrizeCollect WHERE userOpenId="${userOpenId}" AND skuId="${skuId}"`)
+    if(Array.isArray(checkSkuCollect) && checkSkuCollect.length > 0){
+        return ctx.body = outJson(ctx, 20001);
+    }
+
+    //查询状态
+    let requestSuccess = false;
+    let prizeStatus = await fulfillStatus(userOpenId,skuId,ctx);
+    switch (prizeStatus) {
+        case -1:
+            outJson(ctx,20002);
+            break;
+        case 0:
+            outJson(ctx, 50001);
+            break;
+        case 1:
+        case 2:
+            // 已经领取奖励但还没添加记录时添加记录
+            await insertGiftLog(
+                ctx,
+                userOpenId,
+                skuId,
+                parseInt(ctx.cgiStartTime / 1000)
+            );
+            break;
+        default:
+            //还未发起请求
+            try {
+                requestSuccess = await fulfillment(userOpenId, skuId)
+            }catch (err) {
+                return  ctx.body = outJson(ctx, 50001);
+            }
+            if (requestSuccess) {
+                try {
+                    await insertGiftLog(
+                        ctx,
+                        userOpenId,
+                        skuId,
+                        parseInt(ctx.cgiStartTime / 1000)
+                    );
+                    return ctx.body = outJson(ctx, 200);
+                } catch (err) {
+                    return ctx.body = outJson(ctx, 50001);
+                }
+            } else {
+                return ctx.body = outJson(ctx, 50001);
+            }
     }
 }
--- netlify/koa/utils/function.js
+++ netlify/koa/utils/function.js
@@ -1,6 +1,6 @@
-const { outJson } = require('./utils')
-const http = require('./axios')
-const { HTTPURL } = require('../config/Constant')
+const { outJson } = require('./utils');
+const http = require('./axios');
+const { HTTPURL,DATA_KEY} = require('../config/Constant');
 module.exports = {
     async getUserId(tokenData) {
         const { token } = tokenData
@@ -25,5 +25,73 @@ module.exports = {
             ctx.body = outJson(ctx, 40002)
             return;
         }
-    }
+    },
+    async fulfillStatus(userId, skuId,ctx) {
+        if (!userId || skuId !== 'LOL_HEXTECH_CHEST_KEY_SKU_2') {
+            return -1
+        }
+        let url = `${HTTPURL.domainName}/fulfillment/v2/status/${userId}/${skuId}?api_key=${DATA_KEY.apiKey}`;
+        let resp, status;
+        try {
+            resp = await http.get(url)
+        } catch (err) {
+            resp = false;
+            ctx.body = outJson(ctx, 50001)
+            return
+        }
+
+        if (resp === false) {
+            return 0;
+        }
+        if (resp === false || !resp.status) {
+            return 0
+        }
+        // 查询状态 -1 错误参数 0 系统繁忙 1 完成 2 请求中 3 未找到
+        switch (resp.status) {
+            case 'FULFILLED':
+                status = 1;
+                break;
+            case 'REQUESTED':
+                status = 2;
+                break;
+            case 'NOT_FOUND':
+                status = 3;
+                break;
+            default:
+                status = 0;
+                break
+        }
+        return status
+
+    },
+    async fulfillment(userId, skuId) {
+        if (!userId || skuId !== 'LOL_HEXTECH_CHEST_KEY_SKU_2') {
+            return false
+        }
+
+        let url = `${HTTPURL.domainName}/fulfillment/v2/fulfill/${userId}/${skuId}?origin=${DATA_KEY.origin}&api_key=${DATA_KEY.apiKey}`;
+        let resp;
+        try {
+            resp = await http.post(url)
+        } catch (err) {
+            resp = false;
+        }
+
+        if (!resp) return false
+        return typeof resp.status === 'string' && resp.status === 'REQUESTED'
+    },
+    async insertGiftLog(ctx, userOpenId, skuId, time) {
+        let sql = `insert into prePrizeCollect (userOpenId, skuId, createTime) values ("${userOpenId}","${skuId}",${time})`;
+        let insertPrizeStatus;
+        try {
+            insertPrizeStatus = await db.writeMySql(sql)
+        } catch (err) {
+            insertPrizeStatus = false
+        }
+        if (!insertPrizeStatus) {
+            return ctx.body = outJson(ctx, 50001);
+
+        }
+        return ctx.body = outJson(ctx, 200);
+    },
 }
